c9a0b3e4a10055710a006465b247a964adf0bcb7
Matthew Rosen <matthewrosen@Matthews-MacBook-Air.local>
2024-11-14 20:59:35 -0500

    add animation

--- app/api/channels-event/route.tsx
+++ app/api/channels-event/route.tsx
@@ -1,6 +1,6 @@
 import Pusher from "pusher";
-import { NextRequest, NextResponse } from 'next/server';
-import { config } from '../lib/config';
+import { NextRequest, NextResponse } from "next/server";
+import { config } from "../lib/config";
 
 const pusher = new Pusher(config.pusher);
 
@@ -9,25 +9,25 @@ interface MessageRequest {
   // add other fields you expect from the request
 }
 
-let count = 0
+let count = 0;
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const { message } = body as MessageRequest;
-    const response_message = count
-    count = message == 'testing' ? count + 1 : -1
+    const response_message = count;
+    count = message == "testing" ? count + 1 : -1;
 
-    await pusher.trigger('my-channel', 'my-event', {
+    await pusher.trigger("my-channel", "my-event", {
       response_message,
-      timestamp: 'irrelevant'
+      timestamp: "irrelevant",
     });
 
-    return NextResponse.json({ message: 'Message sent' });
+    return NextResponse.json({ message: "Message sent" });
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     return NextResponse.json(
-      { message: 'Error sending message' },
+      { message: "Error sending message" },
       { status: 500 }
     );
   }
